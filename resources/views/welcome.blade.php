5dfab559c4e33212315277a56c30bb6a32cef947
jahmarc <marc@datanet-ch.com>
2018-02-13 10:29:52 +0100

    commit de test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>PEPA - Proches-aidants</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,17 +79,18 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Projet PEPA
                 </div>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                   <a href="https://laracasts.com">Laracasts</a>
+                   <a href="https://laravel-news.com">News</a>
+                   <a href="https://forge.laravel.com">Forge</a>
+                   <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
-                salut les poto
+            <br/>
+                <div class="title">Le projet qui vous donnera envie de répondre aux sondages</div>
             </div>
         </div>
     </body>
